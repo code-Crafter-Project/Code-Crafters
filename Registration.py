85331cbb1a8d1309430a13c4d4ab9d33feec5fec
tshavhanigift <140593017+tshavhanigift@users.noreply.github.com>
2023-09-12 12:20:01 +0200

    Update Registration.py

--- Registration.py
+++ Registration.py
@@ -1,5 +1,6 @@
+# Import relevant libraries (these need to be installed)
+import face_recognition
 import sqlite3
-import datetime
 
 # Create a database connection
 conn = sqlite3.connect("library.db")
@@ -8,71 +9,65 @@ cursor = conn.cursor()
 # Initialize the list of available books
 available_books = ["Book1", "Book2", "Book3"]
 
-# Function to register a new user
-def register_user(username, password, full_name, email):
-    try:
-        cursor.execute("INSERT INTO users (username, password, full_name, email) VALUES (?, ?, ?, ?)",
-                       (username, password, full_name, email))
-        conn.commit()
-        return "User registered successfully."
-    except Exception as e:
-        return f"Error: {e}"
+# Function to authenticate user using face recognition
+def authenticate_face(image_path):
+    # Load the known image of the user
+    known_image = face_recognition.load_image_file("known_face.jpg")
+    known_face_encoding = face_recognition.face_encodings(known_image)[0]
+
+    # Load the user's image for authentication
+    user_image = face_recognition.load_image_file(image_path)
+    user_face_encoding = face_recognition.face_encodings(user_image)[0]
+
+    # Compare face encodings
+    result = face_recognition.compare_faces([known_face_encoding], user_face_encoding)
+
+    return result[0]
 
 # Function to borrow a book
-def borrow_book(user_id, book_title):
-    try:
-        cursor.execute("SELECT availability FROM books WHERE title=?", (book_title,))
-        availability = cursor.fetchone()[0]
-        
-        if availability == 1:
-            due_date = (datetime.datetime.now() + datetime.timedelta(days=14)).strftime("%Y-%m-%d")
-            cursor.execute("UPDATE books SET availability=0, due_date=? WHERE title=?", (due_date, book_title,))
-            cursor.execute("INSERT INTO borrowed_books (user_id, book_title) VALUES (?, ?)", (user_id, book_title,))
-            conn.commit()
-            return f"Book '{book_title}' borrowed successfully. Due date: {due_date}"
-        else:
-            return "Book not available."
-    except Exception as e:
-        return f"Error: {e}"
+def borrow_book(book_title):
+    if book_title in available_books:
+        available_books.remove(book_title)
+        cursor.execute("INSERT INTO borrowed_books (title) VALUES (?)", (book_title,))
+        conn.commit()
+        return "Book borrowed successfully."
+    else:
+        return "Book not available."
+
 
 # Function to return a book
-def return_book(user_id, book_title):
-    try:
-        cursor.execute("SELECT availability FROM books WHERE title=?", (book_title,))
-        availability = cursor.fetchone()[0]
-        
-        if availability == 0:
-            cursor.execute("UPDATE books SET availability=1, due_date=NULL WHERE title=?", (book_title,))
-            cursor.execute("DELETE FROM borrowed_books WHERE user_id=? AND book_title=?", (user_id, book_title,))
-            conn.commit()
-            return f"Book '{book_title}' returned successfully."
-        else:
-            return "Book already returned."
-    except Exception as e:
-        return f"Error: {e}"
+def return_book(book_title):
+    if book_title in available_books:
+        return "Book already returned."
+    else:
+        available_books.append(book_title)
+        cursor.execute("DELETE FROM borrowed_books WHERE title=?", (book_title,))
+        conn.commit()
+        return "Book returned successfully."
 
 # Main program loop
 while True:
-    print("1. Authenticate\n2. Register User\n3. Borrow Book or  Return Book\n4. Exit")
+    print("1. Authenticate\n2. Borrow Book\n3. Return Book\n4. Exit")
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        # Implement authentication here
-        pass
+        image_path = input("Enter path to your image: ")
+        if authenticate_face(image_path):
+            print("Authentication successful.")
+        else:
+            print("Authentication failed.")
 
     elif choice == "2":
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-        full_name = input("Enter full name: ")
-        email = input("Enter e2mail: ")
-        print(register_user(username, password, full_name, email))
+        book_title = input("Enter book title to borrow: ")
+        print(borrow_book(book_title))
+        import GUI
+        from GUI import open_gui
         
+        open_gui()
 
     elif choice == "3":
-        user_id = input("Enter your user ID: ")
-        book_title = input("Enter book title to borrow: ")
-        print(borrow_book(user_id, book_title))
-
+        book_title = input("Enter book title to return: ")
+        print(return_book(book_title))
         import GUI
         from GUI import open_gui
         
@@ -80,6 +75,3 @@ while True:
 
     elif choice == "4":
         break
-
-# Close the database connection
-conn.close()
